53e99cc14f626145d868934001b358a16d4ad6f7
gonzigonz <gonzalo.lucero@jjhonline.com>
2018-09-02 08:58:41 +1000

    Update to IIS DSC

--- IISConfig.ps1
+++ IISConfig.ps1
@@ -1,11 +1,14 @@
 configuration IISConfig {
-
-   Node 'localhost' {
-
+   Node TestWebServerVM {
       WindowsFeature IIS {
          Ensure               = 'Present'
          Name                 = 'Web-Server'
          IncludeAllSubFeature = $true
+      }
+      File DirectoryCreated {
+            Ensure = 'Present'
+            Type = 'Directory'
+            DestinationPath = 'C:\Users\All Users\Desktop\gonzwashere.txt'
       }
    }
 }
