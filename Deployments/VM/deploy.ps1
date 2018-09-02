5741d19151eddf4dc2be81e7f5d4eed9ad9ef507
gonzigonz <gonzalo.lucero@jjhonline.com>
2018-09-02 13:57:35 +1000

    Fix to basic VM powershell - Wasn't passing deploymentName

--- Deployments/VM/deploy.ps1
+++ Deployments/VM/deploy.ps1
@@ -101,7 +101,7 @@ else{
 # Start the deployment
 Write-Host "Starting deployment...";
 if(Test-Path $parametersFilePath) {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath -Name $deploymentName;
 } else {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -Name $deploymentName;
 }
